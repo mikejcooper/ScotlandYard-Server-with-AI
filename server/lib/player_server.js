ce6ba74b4b6d8f2086484825652d22c9ba37f0c9
Michael Cooper <mikejcooper1@gmail.com>
2015-04-13 12:54:56 +0100

    First 20% - Player_Server

--- server/lib/player_server.js
+++ server/lib/player_server.js
@@ -1,30 +1,58 @@
-'use strict'
+'use strict';
 var net = require('net');
 var events = require('events');
 var util = require('util');
 
 
 function PlayerServer() {
+    //reference PlayerSever
+    var player_server = this;
+    //Make Object an EventEmitter ('requiring' events)
+    events.EventEmitter.call(player_server);
+
     this.server = net.createServer(function(player) {
-        //TODO
+        //'on' method expects two arguments (name,function) - Creates event to be emitted
+        //Decode data from input during initialisation.
+        player.on('data', function(data) {
+            var player_data = JSON.parse(data);
+
+            if (player_data.type == 'REGISTER') {
+                player_server.on('register', function(){});
+                if (Boolean(player_data.student_id)) {
+                    var student_id = player_data.student_id;
+                    player_server.on(student_id,function() {});
+                    player_server.emit('register', player, student_id);
+                }
+                else {
+                    player_server.emit('register');
+                }
+            }
+            if (player_data.type == 'MOVE') {
+                player_server.on('move', function () {});
+                player_server.emit('move', player, player_data);
+            }
+        });
     });
 }
+
 util.inherits(PlayerServer, events.EventEmitter);
 
 
 PlayerServer.prototype.onInput = function(player, data) {
-    //TODO
-}
-
+    //console.log('[Fuction called - onInput]');
+};
 
 PlayerServer.prototype.listen = function(port) {
-    //TODO
-}
+    this.server.listen(port);
+   // console.log('[Fuction called - listen]');
+
+};
 
 
 PlayerServer.prototype.close = function() {
-    //TODO
-}
+    this.server.close();
+   // console.log('[Fuction called - close]');
+};
 
 
 module.exports = PlayerServer;
